8f18b795fe633a477281565473140a1c84b1dbb4
erma0x <ermano.buikis@gmail.com>
2022-02-08 18:41:01 +0100

    t

--- binance-lot-size.py
+++ binance-lot-size.py
@@ -14,7 +14,7 @@ from pprint import pprint
 def format_coin_quantity(initial_coin_quantity, symbol = 'ETHUSDT',direction = floor):
     URL = "https://www.binance.com/api/v3/exchangeInfo?symbols=[%22" + str(symbol) + "%22]"
     result = requests.get(URL).json()
-    pprint('RISULTATI ',result)
+    print('RISULTATI ',result)
 
     numbers_after_zero = result['symbols'][0]['filters'][2]['stepSize']
     print('NUMERI DOPO LO ZERO ',numbers_after_zero)
