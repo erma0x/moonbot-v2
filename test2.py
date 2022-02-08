67267571beaca3e75d33bff7a2aba4e008373fc7
erma0x <ermano.buikis@gmail.com>
2022-02-08 19:15:24 +0100

    d

---
+++ test2.py
@@ -0,0 +1,19 @@
+from binance import ThreadedWebsocketManager
+import time 
+symbol = 'BNBBTC'
+
+twm = ThreadedWebsocketManager()
+# start is required to initialise its internal loop
+twm.start()
+
+def handle_socket_message(msg):
+    print(f"message type: {msg['e']}")
+    print(msg)
+    twm.start_kline_socket(callback=handle_socket_message, symbol=symbol)
+
+
+depth_stream_name = twm.start_depth_socket(callback=handle_socket_message, symbol=symbol)
+
+
+time.sleep(10)
+twm.stop_socket(depth_stream_name)
