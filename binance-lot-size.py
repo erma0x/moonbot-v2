d502742bdadfaa04bd8dde4c6b5f82dbb933be35
erma0x <ermano.buikis@gmail.com>
2022-02-08 18:29:01 +0100

    test lot size

--- binance-lot-size.py
+++ binance-lot-size.py
@@ -1,17 +1,19 @@
 from math import ceil, floor
 import requests, json
+
 """
 BINANCE - LOT_SIZE ERROR
 Function to round the value according to decimal places in Binance.
 When placing a purchase order with API you get the LOT_SIZE error.
 To avoid the error you must round the decimal places according to the 'stepSize' of each coin.
 """
+
 symbol = 'ETHUSDT'
 URL = "https://www.binance.com/api/v3/exchangeInfo?symbols=[%22" + str(symbol) + "%22]"
 result = requests.get(URL).json()
 
 
-def fn_round(num, stepSize, direction = floor):
+def round_coin_quantity(num, stepSize, direction = floor):
     zeros = 0
     number = float(stepSize)
     while number < 0.1:
@@ -23,7 +25,6 @@ def fn_round(num, stepSize, direction = floor):
         places = zeros + 1
     return direction(num * (10**places)) / float(10**places)
 
-value = fn_round(324.92839238092803, result['symbols'][0]['filters'][2]['stepSize'])
-
-print(value)
-## output: 324.983
+print('results ',result)
+value = round_coin_quantity(324.92839238092803, result['symbols'][0]['filters'][2]['stepSize'])
+print('value ',value)
