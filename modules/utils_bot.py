5d75845704487cd4a4d8bc8a639cd27533eae8a4
erma0x <ermano.buikis@gmail.com>
2022-02-15 21:40:28 +0100

    fix

--- modules/utils_bot.py
+++ modules/utils_bot.py
@@ -1,15 +1,33 @@
+import requests
+from math import floor
 from datetime import datetime
 from binance.helpers import round_step_size, get_step_size
 from binance import  BinanceSocketManager
 
-async def format_coin_quantity(qnt,symbol):
-    step_size = get_step_size(symbol)
-    formatted_quantity = round_step_size(qnt, step_size) - step_size
-    return formatted_quantity
+# async def format_coin_quantity(qnt,symbol):
+#     step_size = get_step_size(symbol)
+#     formatted_quantity = round_step_size(qnt, step_size) - step_size
+#     return formatted_quantity
 
 def timestamp_to_date(time):
     time = str(time)
     return(datetime.fromtimestamp(int(time[:-3])).strftime('%d-%m-%Y %H:%M:%S'))
+
+async def format_coin_quantity(initial_coin_quantity, symbol = 'ETHUSDT',direction = floor):
+    URL = "https://www.binance.com/api/v3/exchangeInfo?symbols=[%22" + str(symbol) + "%22]"
+    result = await requests.get(URL).json()
+    numbers_after_zero = result['symbols'][0]['filters'][2]['stepSize']
+    zeros = 0
+    number = float(numbers_after_zero)
+    while number < 0.1:
+        number *= 10
+        zeros += 1
+    if float(numbers_after_zero) > 0.1:
+        places = zeros
+    else:
+        places = zeros + 1
+    return direction(initial_coin_quantity * (10**places)) / float(10**places)
+
 
 async def get_data(client,token_pair='BNBUSDT'): ############### NON KLINE MA PREZZO SINGOLO
     bm = BinanceSocketManager(client)
