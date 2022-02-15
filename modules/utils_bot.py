775c16ccef95bc2dc25ed9c50faff87a61c6cd94
erma0x <ermano.buikis@gmail.com>
2022-02-15 21:41:37 +0100

    d

--- modules/utils_bot.py
+++ modules/utils_bot.py
@@ -15,7 +15,7 @@ def timestamp_to_date(time):
 
 async def format_coin_quantity(initial_coin_quantity, symbol = 'ETHUSDT',direction = floor):
     URL = "https://www.binance.com/api/v3/exchangeInfo?symbols=[%22" + str(symbol) + "%22]"
-    result = await requests.get(URL).json()
+    result = requests.get(URL).json()
     numbers_after_zero = result['symbols'][0]['filters'][2]['stepSize']
     zeros = 0
     number = float(numbers_after_zero)
