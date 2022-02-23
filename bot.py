521db69425f9276c733b3d401fabeb071b7f06a4
erma0x <ermano.buikis@gmail.com>
2022-02-23 19:25:30 +0100

    test fix

--- bot.py
+++ bot.py
@@ -16,95 +16,106 @@ import time
 
 async def main():
     """
-    guadagno_assoluto_effettivo = float(SELL_open_orders[i]['origQty'])*(float(SELL_open_orders[i]['price'])-float(prezzo_di_apertura))
+    guadagno_assoluto_effettivo = float(orderbookSELL[i]['origQty'])*(float(orderbookSELL[i]['price'])-float(prezzo_di_apertura))
     guadagno_percentuale_effettivo = guadagno_assoluto_effettivo/investimento*100
     """
     api_key = os.environ.get('binance_api') 
     api_secret = os.environ.get('binance_secret')
     client = await AsyncClient.create(api_key, api_secret)
 
-    investimento = 40 # $
+
+    commissioniSpotMaker = 0.075 # %
+    commissioniSpotTaker = 0.075 # %
+
+    investimento = 20 # $
     prezzo_di_apertura = 0 
     minimo_guadagno_assoluto = 1 # $
-    minimo_guadagno_percentuale = 0.09 # %[0,100]
-    
-    numero_massimo_ordini = 1
-    my_symbol = 'QI'
+    minimo_guadagno_percentuale_buy = 0.09 # %[0,100]
+    minimo_guadagno_percentuale_sell = 0.1 # %[0,100]
 
-    BUY_open_orders = []
-    SELL_open_orders = []
+    numero_massimo_ordini = 1
+    my_symbol = 'ETH'
+
+    orderbookBUY = []
+    orderbookSELL = []
     FILLED_orders = []
 
     while True:
-        priceUSDT = await get_data(client,token_pair=my_symbol+'BUSD')
-        priceBUSD = await get_data(client,token_pair=my_symbol+'USDT')
+        priceBUSD = await get_data(client,token_pair=my_symbol+'BUSD')
+        priceUSDT = await get_data(client,token_pair=my_symbol+'USDT')
+        
         print(my_symbol+'USDT ',priceUSDT,'\t|\t',my_symbol+'BUSD : ',priceBUSD)
         
         lower_price_stablecoin = min(priceUSDT,priceBUSD)
         coin_quantity = investimento/lower_price_stablecoin
-        guadagno_assoluto_stimato = investimento/min(priceUSDT,priceBUSD)*max(priceUSDT,priceBUSD) - investimento
+        guadagno_assoluto_stimato = coin_quantity*max(priceUSDT,priceBUSD) - investimento
         guadagno_percentuale_stimato = guadagno_assoluto_stimato/investimento*100
+        
         print('guadagno stimato: % ',guadagno_percentuale_stimato,': $ ',guadagno_assoluto_stimato)
+
 # BUY TOKEN  
-        if guadagno_percentuale_stimato >= minimo_guadagno_percentuale and numero_massimo_ordini>len(BUY_open_orders):
+        if guadagno_percentuale_stimato+commissioniSpotMaker >= minimo_guadagno_percentuale_buy and numero_massimo_ordini>=len(orderbookBUY):
             if priceUSDT > priceBUSD:
                 buy_stablecoin='BUSD'
 
             if priceUSDT < priceBUSD:
                 buy_stablecoin='USDT'
 
-            buy_symbol = my_symbol+buy_stablecoin 
-            order = await client.create_order(
-                    symbol=buy_symbol,
-                    side=client.SIDE_BUY,
-                    type=client.ORDER_TYPE_MARKET,
-                    quantity = await format_coin_quantity(coin_quantity, symbol = buy_symbol))
+            buy_symbol = my_symbol + buy_stablecoin
 
+
+            order = await client.order_limit_buy(timeInForce='GTC',
+                                symbol = buy_symbol,
+                                quantity = await format_coin_quantity(coin_quantity, symbol = buy_symbol),
+                                price = round(float(min(priceBUSD,priceUSDT)),2))
+
+
+            order['prezzo_di_apertura_buy'] = min(priceBUSD,priceUSDT)
             print_OPEN(order)
-            BUY_open_orders.append(order)
+            orderbookBUY.append(order)
 
 # SELL TOKEN 
-        time.sleep(1)
-        if len(BUY_open_orders)>0:
-            sell_priceUSDT = await get_data(client,token_pair=my_symbol+'USDT')
-            sell_priceBUSD = await get_data(client,token_pair=my_symbol+'BUSD')
-            print('SELLING : ',my_symbol+'USDT ',sell_priceUSDT,'\t|\t',my_symbol+'BUSD : ',sell_priceBUSD)
-        
-            if sell_priceUSDT < sell_priceBUSD:
-                sell_stablecoin='BUSD'
+        if len(orderbookBUY)>0:
+            for i in range(len(orderbookBUY)):
 
-            if sell_priceUSDT > sell_priceBUSD:
-                sell_stablecoin='USDT'
+                if orderbookBUY[i]['status']=='FILLED':
 
-            for i in range(len(BUY_open_orders)):
-                if BUY_open_orders[i]['status']=='FILLED':
+                    priceUSDT = await get_data(client,token_pair=my_symbol+'USDT')
+                    priceBUSD = await get_data(client,token_pair=my_symbol+'BUSD')
+                    print('SELLING : ',my_symbol+'USDT ',priceUSDT,'\t|\t',my_symbol+'BUSD : ',priceBUSD)
+                
+                    if priceUSDT < priceBUSD:
+                        sell_stablecoin='BUSD'
+
+                    if priceUSDT > priceBUSD:
+                        sell_stablecoin='USDT'
                     
                     sell_symbol = deepcopy(my_symbol+sell_stablecoin)
-                    # order = await client.create_sell_limit(
-                    #     symbol=sell_symbol,
-                    #     side=client.SIDE_SELL,
-                    #     type=client.ORDER_TYPE_MARKET,
-                    #     quantity=await format_coin_quantity(coin_quantity, symbol = my_symbol+sell_stablecoin))
+                    prezzo_minimo_vendita = max(priceUSDT,priceBUSD)
+                    prezzo_di_apertura = orderbookBUY['prezzo_di_apertura_buy']
+                    
+                                                                 # 20 =>        prezzo di SELL  >= 1.2 * prezzo di BUY
+                    if prezzo_minimo_vendita >= (( (minimo_guadagno_percentuale_sell+commissioniSpotMaker)/100) + 1) * prezzo_di_apertura :
 
-                    order = await client.order_limit_sell(timeInForce='GTC',
-                                        symbol = sell_symbol,
-                                        quantity = await format_coin_quantity(coin_quantity,symbol=sell_symbol),
-                                        price = round(float(max(sell_priceUSDT,sell_priceBUSD)),2)) # round 2, or 4
+                        order = await client.order_limit_sell(timeInForce='GTC',
+                                            symbol = sell_symbol,
+                                            quantity = await format_coin_quantity(coin_quantity,symbol=sell_symbol),
+                                            price = round(float(prezzo_minimo_vendita),2)) # round 2, or 4
 
 
                     print_OPEN(order)
                     print_FILLED(order)
 
-                    SELL_open_orders.append(order)
-                    #FILLED_orders.append(BUY_open_orders[i])
-                    BUY_open_orders.remove(BUY_open_orders[i])
+                    orderbookSELL.append(order)
+                    #FILLED_orders.append(orderbookBUY[i])
+                    orderbookBUY.remove(orderbookBUY[i])
 
-        if len(SELL_open_orders)>0:
-            for i in range(len(SELL_open_orders)):
-                if SELL_open_orders[i]['status']=='FILLED':
-                    #FILLED_orders.append(SELL_open_orders[i])
-                    print_FILLED(SELL_open_orders[i])
-                    SELL_open_orders.remove(SELL_open_orders[i])
+        if len(orderbookSELL)>0:
+            for i in range(len(orderbookSELL)):
+                if orderbookSELL[i]['status']=='FILLED':
+                    #FILLED_orders.append(orderbookSELL[i])
+                    print_FILLED(orderbookSELL[i])
+                    orderbookSELL.remove(orderbookSELL[i])
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
